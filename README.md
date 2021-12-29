f106991412e82232522643150529b341b8486433
seows <seows@laivdata.com>
2021-12-29 22:16:36 +0900

    프로젝트 초기 설정

---
+++ README.md
@@ -0,0 +1,3 @@
+# 리액트로 메모리 게임 구현하기
+
+- 참고: https://www.youtube.com/watch?v=XcHNBMG1GQI&list=RDCMUCW5YeuERMmlnqo4oq8vwUpg&index=2
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
