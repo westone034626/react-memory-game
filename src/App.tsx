3640312ba1346177bbdf8b611bbefdc58c23edd3
seows <seows@laivdata.com>
2021-12-30 15:15:21 +0900

    cardImages를 토대로 12개의 Card 데이터를 만들어서 cards라는 state로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
+const cardImages = [
+  { src: '../img/helmet-1.png' },
+  { src: '../img/potion-1.png' },
+  { src: '../img/ring-1.png' },
+  { src: '../img/scroll-1.png' },
+  { src: '../img/shield-1.png' },
+  { src: '../img/sword-1.png' },
+];
+
+interface CardProps {
+  src: string;
+  id: number;
+}
+
 function App() {
+  const [cards, setCards] = useState<CardProps[]>([]);
+  const [turns, setTurns] = useState<number>(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => ({ ...card, id: index }));
+    setCards(shuffledCards);
+    setTurns((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className={styles.App}>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
