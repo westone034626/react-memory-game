515f450634d69972be699faf7d7f7005df229110
서원석 <seows@laivdata.com>
2022-01-01 01:48:50 +0900

    Modal 컴포넌트의 prop으로 close 버튼 활성화를 결정하는 onClose 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,12 +4,20 @@ import styles from './Modal.module.css';
 interface ModalProps {
   isOpen: boolean;
   children: ReactElement;
+  onClose?: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, children, onClose }) => {
   return isOpen ? (
     <div className={styles.layer}>
-      <div className={styles.modalContainer}>{children}</div>
+      <div className={styles.modalContainer}>
+        {onClose && (
+          <div onClick={onClose} className={styles.closeButton}>
+            X
+          </div>
+        )}
+        {children}
+      </div>
     </div>
   ) : null;
 };
